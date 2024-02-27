b8558f8ca32ce0213e7a993f9d03ddcd3d538c1f
SiPilip <philifs.74hutar@gmail.com>
2024-02-27 13:25:26 +0700

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,114 @@
+import unsri from "../../public/unsri.png";
+import user from "../../public/icons/user.svg";
+import lock from "../../public/icons/lock.svg";
+import { Link } from "react-router-dom";
+import Register from "./Register";
+
+export default function Login() {
+  return (
+    <div className="bg-gray-100 w-screen h-screen flex justify-center items-center">
+      <div className="w-[500px] shadow-lg bg-white py-10 ">
+        <div className=" flex flex-wrap justify-center">
+          {/* LOGO */}
+          <div className="w-full flex justify-center flex-wrap">
+            <img src={unsri} alt="" />
+            <div className="w-full text-center">
+              <h1 className="text-xl font-bold mt-3">SIWI UNSRI</h1>
+              <p className="text-sm">
+                Sistem Informasi Akademik Universitas Sriwijaya
+              </p>
+            </div>
+          </div>
+          {/* LOGO */}
+
+          {/* LOGIN */}
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Nomor Induk Mahasiswa
+                </label>
+                <div className="mt-2 w-full flex w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 px-3 py-2">
+                  <img src={user} alt="" className="mr-2" />
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    placeholder="xxxxxxxxxxx"
+                    className="focus:outline-none w-full"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Kata Sandi
+                  </label>
+                  <div className="text-sm">
+                    <a
+                      href="#"
+                      className="font-semibold text-neutral-900 hover:text-black text-xs"
+                    >
+                      Lupa Password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <div className="mt-2 w-full flex w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 px-3 py-2">
+                    <img src={lock} alt="" className="mr-2" />
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="focus:outline-none w-full"
+                      placeholder="Kata sandi anda"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-unsri px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-unsrihover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-unsrihover transition-all duration-300 ease-in-out"
+                >
+                  Login
+                </button>
+                <Link to="/register">
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-transparent border-unsri border-[1px] mt-3 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-unsrihover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-unsrihover transition-all duration-300 ease-in-out"
+                  >
+                    Registrasi
+                  </button>
+                </Link>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Masalah teknis?
+              <a
+                href="#"
+                className="font-semibold leading-6 text-neutral-800 hover:text-black ml-1"
+              >
+                Hubungi admin.
+              </a>
+            </p>
+          </div>
+          {/* LOGIN */}
+        </div>
+      </div>
+    </div>
+  );
+}
