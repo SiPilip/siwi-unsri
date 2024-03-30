3070218f38b1914a616aaecfc8b4d64087eb94d5
SiPilip <philifs.74hutar@gmail.com>
2024-03-30 23:37:19 +0700

    kasih lazy state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import AppLayout from "./ui/AppLayout";
-import Register from "./pages/Register";
-import Beranda from "./features/user/beranda/Beranda";
-import Login from "./pages/Login";
-import DataPendaftar from "./pages/DataPendaftar";
-import ProtectedRoute from "./features/authentication/ProtectedRoute";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import DataAkademik from "./pages/DataAkademik";
-import USEPT from "./pages/USEPT";
-import FormulirPendaftaran from "./pages/FormulirPendaftaran";
-import TranskripNilai from "./pages/TranskripNilai";
-import PageNotFound from "./ui/PageNotFound";
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import SpinnerFullPage from "./ui/SpinnerFullPage";
+import ProtectedRoute from "./features/authentication/ProtectedRoute";
+
+// PAGES
+// import PageNotFound from "./ui/PageNotFound";
+// import AppLayout from "./ui/AppLayout";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Beranda from "./features/user/beranda/Beranda";
+// import DataPendaftar from "./pages/DataPendaftar";
+// import DataAkademik from "./pages/DataAkademik";
+// import TranskripNilai from "./pages/TranskripNilai";
+// import USEPT from "./pages/USEPT";
+// import FormulirPendaftaran from "./pages/FormulirPendaftaran";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,6 +26,17 @@ const queryClient = new QueryClient({
     },
   },
 });
+
+const PageNotFound = lazy(() => import("./ui/PageNotFound"));
+const AppLayout = lazy(() => import("./ui/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Beranda = lazy(() => import("./features/user/beranda/Beranda"));
+const DataPendaftar = lazy(() => import("./pages/DataPendaftar"));
+const DataAkademik = lazy(() => import("./pages/DataAkademik"));
+const TranskripNilai = lazy(() => import("./pages/TranskripNilai"));
+const USEPT = lazy(() => import("./pages/USEPT"));
+const FormulirPendaftaran = lazy(() => import("./pages/FormulirPendaftaran"));
 
 export default function App() {
   return (
